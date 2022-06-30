673ab32a624b43ad1bcc41a9036aebee22562732
Jerome <jeromeisnthere@gmail.com>
2022-06-30 21:30:12 +0800

    Added the ability to change pill colours

--- Assets/Scripts/Pills/Pill.cs
+++ Assets/Scripts/Pills/Pill.cs
@@ -9,6 +9,35 @@ public class Pill : MonoBehaviour
     [SerializeField]
     private float thisProgressCap;
 
+    private SpriteRenderer sr;
+
+    private enum ColorType
+    {
+        Red,Green,Blue
+    }
+
+    [SerializeField]
+    private ColorType colourType;
+
+    void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        switch (colourType)
+        {
+            case ColorType.Red:
+                sr.color = Color.red;
+                break;
+            case ColorType.Green:
+                sr.color = Color.green;
+                break;
+            case ColorType.Blue:
+                sr.color = Color.blue;
+                break;
+            default:
+                break;
+        }
+    }
+
     public float healAmount { get { return thisHealAmount; } }
     public float progressAmount {get {return thisProgressAmount; } }
     public float progressCap { get { return thisProgressCap; } }
